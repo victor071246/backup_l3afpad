f96e03962e6580be3c4cc0ab571e7555b6215266
v <daderiovictorfelipe@gmail.com>
2026-02-20 23:56:01 -0300

    começando o projeto

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use std::time::Duration;
+use tokio::time;
+
+#[tokio::main]
+async fn main() {
+    println!("Leafguard iniciando...");
+
+    let mut interval = time::interval(Duration::from_secs(5 * 60));
+
+    loop {
+        interval.tick().await;
+        println!("Executando clico");
+    }
 }
