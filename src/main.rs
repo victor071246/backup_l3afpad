446e50002dac79e856b2152deb932e968b81d59e
v <daderiovictorfelipe@gmail.com>
2026-02-21 22:36:55 -0300

    Detecta o leafpad e salva o control S através do ydotool

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
 use std::time::Duration;
 use tokio::time;
+use sysinfo::System;
+use std::process::Command;
+
+fn ins_process_running(name: &str) -> bool {
+    let mut sys = System::new_all();
+    sys.refresh_all();
+    sys.processes().values().any(|p| p.name().to_str().unwrap_or("").contains(name))
+}
+
+fn force_save() {
+    match Command::new("ydotool")
+        .args(["key", "ctrl+s"])
+        .spawn()
+        {
+            Ok(_) => println!("Control + S efetuado"),
+            Err(e) => println!("Erro ao salvar control + S: {}", e)
+        }
+}
 
 #[tokio::main]
 async fn main() {
@@ -9,6 +27,16 @@ async fn main() {
 
     loop {
         interval.tick().await;
-        println!("Executando clico");
+        println!("Executando ciclo");
+
+        if !ins_process_running("l3afpad") {
+            println!("O l3afpad não foi encontrado, pulando...");
+            continue;
+        }
+
+        println!("l3afpad detectado");
+        force_save();
     }
 }
+
+// ydotool # insalado via pacman
